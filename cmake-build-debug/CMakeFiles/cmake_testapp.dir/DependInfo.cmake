81df9be044b08d2964e02581e899cf45a3c954e4
oIITiMicT <backawork@gmail.com>
2021-04-05 14:32:06 +0200

    version 0.1

---
+++ cmake-build-debug/CMakeFiles/cmake_testapp.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "C:/Users/OMEN/CLionProjects/game_of_life/src/alloc.c" "C:/Users/OMEN/CLionProjects/game_of_life/cmake-build-debug/CMakeFiles/cmake_testapp.dir/src/alloc.c.obj"
+  "C:/Users/OMEN/CLionProjects/game_of_life/src/evolve.c" "C:/Users/OMEN/CLionProjects/game_of_life/cmake-build-debug/CMakeFiles/cmake_testapp.dir/src/evolve.c.obj"
+  "C:/Users/OMEN/CLionProjects/game_of_life/src/game.c" "C:/Users/OMEN/CLionProjects/game_of_life/cmake-build-debug/CMakeFiles/cmake_testapp.dir/src/game.c.obj"
+  "C:/Users/OMEN/CLionProjects/game_of_life/src/generator.c" "C:/Users/OMEN/CLionProjects/game_of_life/cmake-build-debug/CMakeFiles/cmake_testapp.dir/src/generator.c.obj"
+  "C:/Users/OMEN/CLionProjects/game_of_life/src/gifenc.c" "C:/Users/OMEN/CLionProjects/game_of_life/cmake-build-debug/CMakeFiles/cmake_testapp.dir/src/gifenc.c.obj"
+  "C:/Users/OMEN/CLionProjects/game_of_life/src/main.c" "C:/Users/OMEN/CLionProjects/game_of_life/cmake-build-debug/CMakeFiles/cmake_testapp.dir/src/main.c.obj"
+  "C:/Users/OMEN/CLionProjects/game_of_life/src/nieghbors.c" "C:/Users/OMEN/CLionProjects/game_of_life/cmake-build-debug/CMakeFiles/cmake_testapp.dir/src/nieghbors.c.obj"
+  "C:/Users/OMEN/CLionProjects/game_of_life/src/solver.c" "C:/Users/OMEN/CLionProjects/game_of_life/cmake-build-debug/CMakeFiles/cmake_testapp.dir/src/solver.c.obj"
+  "C:/Users/OMEN/CLionProjects/game_of_life/src/tester.c" "C:/Users/OMEN/CLionProjects/game_of_life/cmake-build-debug/CMakeFiles/cmake_testapp.dir/src/tester.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/cmake_testapp.dir/cmake_clean.cmake
@@ -0,0 +1,20 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cmake_testapp.dir/src/alloc.c.obj"
+  "CMakeFiles/cmake_testapp.dir/src/evolve.c.obj"
+  "CMakeFiles/cmake_testapp.dir/src/game.c.obj"
+  "CMakeFiles/cmake_testapp.dir/src/generator.c.obj"
+  "CMakeFiles/cmake_testapp.dir/src/gifenc.c.obj"
+  "CMakeFiles/cmake_testapp.dir/src/main.c.obj"
+  "CMakeFiles/cmake_testapp.dir/src/nieghbors.c.obj"
+  "CMakeFiles/cmake_testapp.dir/src/solver.c.obj"
+  "CMakeFiles/cmake_testapp.dir/src/tester.c.obj"
+  "cmake_testapp.exe"
+  "cmake_testapp.exe.manifest"
+  "cmake_testapp.pdb"
+  "libcmake_testapp.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/cmake_testapp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
